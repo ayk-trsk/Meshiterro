1b82af9b1764dc30689cbe8b18d8b1dd5dccee5f
ayk-trsk <1219noppoppo@gmail.com>
2021-05-07 05:59:19 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cf9f8c381730fe2031eea0053130e0fb0a805fdc478ae2cf4f79afa0a614eabe39a05c2a8397f5622d115b7d5ad30245501dd4ad79f1fc8725391f8de04ead6a'
